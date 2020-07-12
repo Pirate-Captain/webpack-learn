85e9d67f8f72aa0f388375cc519e75f7dd7daaeb
Pirate-Captain <1531515382@qq.com>
2020-07-12 16:37:59 +0800

    plugin

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const extractTextWebPackPlugin = require("extract-text-webpack-plugin");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
@@ -9,6 +10,9 @@ module.exports = {
     output: {
         path: __dirname + "/public",
         filename: "bundle-[hash].js"
+    },
+    optimization: {
+        minimize: true
     },
     devServer: {
         contentBase: './public',//本地服务器所加载的页面所在的目录
@@ -38,6 +42,10 @@ module.exports = {
         new webpack.BannerPlugin("版权所有，翻版必究"),
         new htmlWebpackPlugin({
             template: __dirname + "/app/index.tmpl.html"
+        }),
+        new miniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
         }),
         new webpack.optimize.OccurrenceOrderPlugin(),
         new extractTextWebPackPlugin("[name]-[hash].css"),
